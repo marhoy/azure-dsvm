9ea2e605e8428de985500e0a7d4ed6614721e963
Martin Høy <martin.hoy@dnvgl.com>
2019-08-02 13:57:57 +0000

    Update

--- anaconda_install.sh
+++ anaconda_install.sh
@@ -4,8 +4,7 @@ INSTALL_PATH=/data/miniconda
 ANACONDA_VERSION=2019.07
 
 # Download and install the latest version of Miniconda
-curl -o Miniconda.sh -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Li
-nux-x86_64.sh
+curl -o Miniconda.sh -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda.sh -b -f -p $INSTALL_PATH
 rm Miniconda.sh
 
@@ -14,39 +13,55 @@ source ${INSTALL_PATH}/etc/profile.d/conda.sh
 conda activate base
 conda update -y conda
 
+# Update global conda.sh
+sudo ln -sf ${INSTALL_PATH}/etc/profile.d/conda.sh /etc/profile.d/conda.sh
+
 # Create environment for running jupyterhub
 conda create -y -n jupyterhub -c defaults -c conda-forge \
-        jupyterhub \
-        sudospawner \
-        jupyter_contrib_nbextensions \
-        ipywidgets \
-        ipyleaflet \
-        nbdime \
-        rise \
-        git
+	jupyterhub \
+	sudospawner \
+	jupyter_contrib_nbextensions \
+	ipywidgets \
+	ipyleaflet \
+	nbdime \
+	rise \
+	git
 
 # Create environment for running Data Science projects
 conda create -y -n anac_${ANACONDA_VERSION} -c defaults -c conda-forge \
-        anaconda=${ANACONDA_VERSION} \
-        keras \
-        tensorflow-gpu \
-        lightgbm \
-        xgboost \
-        plotly \
-        dash \
-        ipyleaflet \
-        folium \
-        gmaps \
-        tqdm \
-        conda-build \
-        setuptools_scm \
-        pypandoc \
-        docopt \
-        git \
-        cookiecutter \
-        connexion \
-        geopy \
-        python-geohash \
-        gdal \
-        pyshp \
-        shapely \
+	anaconda=${ANACONDA_VERSION} \
+	keras \
+	tensorflow-gpu \
+	lightgbm \
+	xgboost \
+	plotly \
+	dash \
+	ipyleaflet \
+	folium \
+	gmaps \
+	tqdm \
+	conda-build \
+	setuptools_scm \
+	pypandoc \
+	docopt \
+	git \
+	cookiecutter \
+	connexion \
+	geopy \
+	python-geohash \
+	gdal \
+	pyshp \
+	shapely \
+
+# Add the kernel from the above environment to the Jupyter list
+echo "Adding new kernel to JupyterHub"
+KERNEL_DIR=${INSTALL_PATH}/envs/anac_${ANACONDA_VERSION}/share/jupyter/kernels/python3
+TMP_KERNEL=`mktemp`
+jq --arg name "Anaconda ${ANACONDA_VERSION}" '
+        .display_name |= $name
+' ${KERNEL_DIR}/kernel.json > ${TMP_KERNEL} && mv ${TMP_KERNEL} ${KERNEL_DIR}/kernel.json
+
+conda activate jupyterhub
+jupyter-kernelspec install ${KERNEL_DIR} --name "anac_${ANACONDA_VERSION}" --prefix ${INSTALL_PATH}/envs/jupyterhub/
+
+
--- change_jupyterhub.sh
+++ change_jupyterhub.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
 SERVICE_FILE=/lib/systemd/system/jupyterhub.service
+JUPYTER_CFG=/etc/jupyterhub/jupyterhub_config.py
+JUPYTERHUB_ENV=/data/miniconda/envs/jupyterhub
 
 if [ -f $SERVICE_FILE ]; then
-        echo "Changing systemd-file ${SERVICE_FILE}"
-        sudo perl -pi -e 's#^ExecStart.*#ExecStart=/data/miniconda/envs/jupyterhub/bin/jupyterhub --log-file=/var/log/jupyterhub.log#' ${SERVICE_FILE}
+	echo "Changing systemd-file ${SERVICE_FILE}"
+	sudo perl -pi -e 's#^ExecStart.*#ExecStart=/data/miniconda/envs/jupyterhub/bin/jupyterhub --log-file=/var/log/jupyterhub.log#' ${SERVICE_FILE}
 else
-        echo "File not found: ${SERVICE_FILE}"
+	echo "File not found: ${SERVICE_FILE}"
 fi
+
+TMP_FILE=`mktemp`
+${JUPYTERHUB_ENV}/bin/jupyterhub --generate-config -f $TMP_FILE
+
+cat <<EOT >> ${TMP_FILE}
+
+# listen on all IP addresses to bypass name resolution. This fixes
+# an issue on Azure where, at first boot, the hostname needs to be updated
+# by waagent; if JupyterHub tries to resolve its hostname before then,
+# the service will fail to start
+c.JupyterHub.ip = '0.0.0.0'
+
+# Path to SSL key file for the public facing interface of the proxy
+#
+c.JupyterHub.ssl_key = '/etc/jupyterhub/srv/server.key'
+c.JupyterHub.ssl_cert = '/etc/jupyterhub/srv/server.crt'
+
+# Extra arguments to be passed to the single-user server
+#c.Spawner.args = ['--config=/etc/jupyterhub/default_jupyter_config.py']
+
+# The command used for starting notebooks.
+c.Spawner.cmd = ['${JUPYTERHUB_ENV}/bin/jupyterhub-singleuser']
+EOT
+
+echo "Moving $TMP_FILE to $JUPYTER_CFG"
+sudo mv $TMP_FILE $JUPYTER_CFG
+
+
+
+echo "Restarting Jupyter Hub"
+sudo systemctl daemon-reload
+sudo systemctl stop jupyterhub
+sudo systemctl start jupyterhub
