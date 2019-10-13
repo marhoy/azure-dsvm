dfdecb0d3cadab5c2c01435e85038023f0f94c5b
Martin Høy <martin.hoy@dnvgl.com>
2019-10-13 14:18:09 +0000

    Add our own systemd unit file

--- change_jupyterhub.sh
+++ change_jupyterhub.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-SERVICE_FILE=/lib/systemd/system/jupyterhub.service
+SYSTEMD_DIR=/lib/systemd/system/
 JUPYTER_CFG=/etc/jupyterhub
 JUPYTERHUB_ENV=/data/miniconda/envs/jupyterhub
-
-if [ -f $SERVICE_FILE ]; then
-	echo "Changing systemd-file ${SERVICE_FILE}"
-	sudo perl -pi -e 's#^ExecStart.*#ExecStart=/data/miniconda/envs/jupyterhub/bin/jupyterhub --log-file=/var/log/jupyterhub.log#' ${SERVICE_FILE}
-else
-	echo "File not found: ${SERVICE_FILE}"
-fi
 
 echo "Copying configuration files to $JUPYTER_CFG"
 sudo cp jupyterhub_config.py $JUPYTER_CFG
 sudo cp jupyter_notebook_config.py $JUPYTER_CFG
+
+echo "Replacing systemd unit file"
+sudo cp jupyterhub.service $SYSTEMD_DIR
+sudo rm -f /var/log/jupyterhub.log
 
 echo "Restarting Jupyter Hub"
 sudo systemctl daemon-reload
